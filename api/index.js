52dcf1a2f546812ee793ec72807286f41fa15a1b
supriya <supriyakandra924@gmail.com>
2024-04-29 15:25:29 +0530

    hello

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(`https://api.render.com/deploy/srv-conmkrq1hbls73flt2c0?key=Qk_ZvF2Rvj4`, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
